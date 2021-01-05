19546c3de4b2fca5bde5c4de675484aa74f61ac2
an0304073@naver.com <an0304073@naver.com>
2021-01-05 21:07:58 +0900

    [ADD] MainPage Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import { Main } from "pages";
 
 const App: React.FC = () => {
-  return <div>React with Typescript</div>;
+  return (
+    <React.Fragment>
+      <Route exact path="/" component={Main} />
+    </React.Fragment>
+  );
 };
+
 export default App;
