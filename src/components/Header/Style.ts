a92c462a725fe9f627d7b8bf9b1242bab1713367
an0304073@naver.com <an0304073@naver.com>
2021-01-06 00:38:26 +0900

    [UPDATE] Reactive Header

--- src/components/Header/Style.ts
+++ src/components/Header/Style.ts
@@ -6,16 +6,18 @@ export const Positioner = styled.div`
   border-bottom: 1px solid #d6d6d6;
   background-color: #ffffff;
   top: 0px;
+  left: 0px;
   width: 100%;
   z-index: 30;
 `;
 
 export const ContentWrapper = styled.div`
   display: flex;
+  margin: 0px auto;
   flex-direction: row;
   justify-content: center;
   align-items: center;
-  height: 8vh;
+  height: 70px;
 `;
 
 export const Logo = styled.div`
