569dbee889438fc2151395b9e89c78b296e0ce4e
an0304073@naver.com <an0304073@naver.com>
2021-01-05 19:28:43 +0900

    React setting with Typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { GlobalStyle, PageBody } from "styles/GlobalStyle";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <GlobalStyle />
+      <PageBody>
+        <App />
+      </PageBody>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from "styled-components";
+import styled from "styled-components";
+import { normalize } from "styled-normalize";
+
+export const GlobalStyle = createGlobalStyle`
+    ${normalize}
+
+    html,
+    body {
+        overflow: hidden;
+    }
+
+    input:focus,
+    select:focus,
+    textarea:focus,
+    button:focus {
+        outline: none;
+    }
+
+    button {
+        cursor: pointer;
+        outline: none;
+    }
+`;
+
+export const PageBody = styled.div`
+  display: flex;
+`;
